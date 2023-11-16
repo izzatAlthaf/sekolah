eccb23d9d6a8f5f35b400a29e950c94140a48c1a
izzatAlthaf <izzatalthaf12@gmail.com>
2023-11-16 23:57:02 +0700

    menambah galeri dan footer

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Ham List
 const mainTogle = document.querySelector(".main-togle input");
-const nav = document.querySelector("nav ul")
+const nav = document.querySelector("nav ul");
 
 mainTogle.addEventListener("click", function(){
     nav.classList.toggle('slide')
-})
+});
 
 // Scroll Animated
 

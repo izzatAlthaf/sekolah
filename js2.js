5dd69754a451156afd82bac3b50920d855aab594
izzatAlthaf <izzatalthaf12@gmail.com>
2023-11-17 22:46:12 +0700

    menambahkan tentang, kontak

---
+++ js2.js
@@ -0,0 +1,7 @@
+// Ham List
+const mainTogle = document.querySelector(".main-togle input");
+const nav = document.querySelector("nav ul");
+
+mainTogle.addEventListener("click", function(){
+    nav.classList.toggle('slide')
+});
